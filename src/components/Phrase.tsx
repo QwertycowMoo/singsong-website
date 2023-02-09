23d1e4d85daf09bf3220c938e7a8583b2370fc99
Kevin Zhou <kevinzhou721@gmail.com>
2023-02-08 18:34:18 -0600

    add tutorial

---
+++ src/components/Phrase.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Grid } from "@mui/material"
+import { Box } from "@mui/system"
+
+import phraseExample from "../images/phraseExample.png"
+const Phrase = () => (
+    <img src={phraseExample}/>
+)
+
+export default Phrase
--- src/pages/datacollect.tsx
+++ src/pages/datacollect.tsx
@@ -1,33 +1,72 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { useState } from "react"
 import { Button, Grid, Typography } from "@mui/material"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { AudioRecorder } from "react-audio-voice-recorder"
 import { navigate } from "gatsby"
+import CenterGrid from "../components/CenterGrid"
+import Phrase from "../components/Phrase"
+
+import saveInstructions from "../images/saveInstructions.png"
+import phraseExample from "../images/phraseExample.png"
 
 function DataCollectPage(props) {
+    const [showTutorial, setShowTutorial] = useState(true)
+    const [urlBlob, setUrlBlob] = useState("")
     const handleFinishClicked = () => {
         navigate("/summary")
     }
+
+    const addAudioElement = (blob) => {
+        const url = URL.createObjectURL(blob)
+        setUrlBlob(url)
+        console.log(urlBlob)
+        const audio = document.createElement("audio")
+        audio.src = urlBlob;
+        audio.controls = true;
+        document.body.appendChild(audio);
+      };
+
+    const handleCloseTutorial = () => {
+        setShowTutorial(false)
+    }
+
     return (
         <Layout>
-        <Grid container 
-            sx={{justifyContent: "center"}}
-            direction="column">
-            <Grid item>
-                <Typography variant="h1">
-                    Hello {props.location.state.name}
+            <CenterGrid>
+                {showTutorial ?
+                <>
+                <Typography variant="h3">
+                Hello {props.location.state?.name}!
                 </Typography>
-            </Grid>
-            <Grid item>
-                <AudioRecorder></AudioRecorder>
+                <Typography variant="body1" paragraph={true} textAlign="center">
+                    The first thing you'll see is a phrase of music. If you see any numbers, just ignore them, they're for us to keep track of phrases.
+                </Typography>
+                <img src={phraseExample}/>
+                <Typography variant="body1" paragraph={true} textAlign="center">
+                    To start recording, click the microphone button. When done, click the save icon on the top left of the recording box.
+                </Typography>
+                <img src={saveInstructions}/>
+                <Typography variant="body1" paragraph={true} textAlign="center">
+                    Listen back to your recording, and retry if you need to. We want to get results as accurate as possible, so we really appreciate your time getting things right!
+                </Typography>
+                <Button size="large" variant="outlined" onClick={handleCloseTutorial}>Let's Go!</Button>
+                </>
+                :
+                <>
+                <Phrase></Phrase>
+                <AudioRecorder onRecordingComplete={(blob) => addAudioElement(blob)}></AudioRecorder>
                 {/* Use Wavesurfer.js to visualize and playback audio? */}
-            </Grid>
-            <Grid item>
                 <Button size="large" variant="outlined" onClick={handleFinishClicked}>Finish</Button>
-            </Grid>
-        </Grid>
+                </>
+                }
+                
+                
+            </CenterGrid>
+        
+                
+
         </Layout>
         
     
