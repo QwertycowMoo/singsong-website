f4985b4e0695b03c68177b7e4a7629ed8af03bb8
Kevin Zhou <kevinzhou721@gmail.com>
2023-04-08 18:40:56 -0500

    working on gcloud backend

--- src/api/Client.tsx
+++ src/api/Client.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BASE_URL =
-  "http://127.0.0.1:5000";
+  "https://singsong-8690d.uc.r.appspot.com";
 
 export default axios.create({
   baseURL: BASE_URL,
